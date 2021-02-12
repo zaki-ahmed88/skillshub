d64af072e040b89b29d15929229031de58c696bb
zaki-ahmed88 <zaki.ahmed88@gmail.com>
2021-02-13 00:32:27 +0200

    first commit

---
+++ app/Models/cat.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use\Illuminate\Support\Facades\App;
+
+class cat extends Model
+{
+    use HasFactory;
+
+
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+
+    public function skills(){
+        return $this->hasMany(Skill::class);
+    }
+
+
+
+
+    public function name($lang = null){
+
+           $lang = $lang ?? App::getLocale();
+           return json_decode($this->name)->$lang; 
+        
+
+    }
+
+
+    public function scopeActive($query)
+    {
+        return $query->where('active', 1);
+    }
+}
